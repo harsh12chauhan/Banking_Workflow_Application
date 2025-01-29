17e5fed1c91149798bb4498536c651bd41396115
harsh12chauhan <harshchauhan0225@gmail.com>
2025-01-29 09:14:08 +0530

    initial spring boot app

---
+++ src/main/java/com/bankingworkflowapp/BankingWorkFlowAppApplication.java
@@ -0,0 +1,13 @@
+package com.bankingworkflowapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingWorkFlowAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingWorkFlowAppApplication.class, args);
+	}
+
+}
