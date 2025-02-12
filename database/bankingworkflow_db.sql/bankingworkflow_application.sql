cf6659a16a95cfba1c7ff961b238fa1d97b15952
harsh12chauhan <harshchauhan0225@gmail.com>
2025-02-12 20:10:44 +0530

    added database files for initial setup and backup.

---
+++ database/bankingworkflow_db.sql/bankingworkflow_application.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bankingworkflow
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `application`
+--
+
+DROP TABLE IF EXISTS `application`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `application` (
+  `annualincome` double DEFAULT NULL,
+  `applicationid` int NOT NULL AUTO_INCREMENT,
+  `assignto` int DEFAULT NULL,
+  `completiondate` date DEFAULT NULL,
+  `intrestrate` double DEFAULT NULL,
+  `loanamount` double DEFAULT NULL,
+  `loanduration` int DEFAULT NULL,
+  `submissiondate` date DEFAULT NULL,
+  `userid` int NOT NULL,
+  `occupation` varchar(255) DEFAULT NULL,
+  `applicationstatus` enum('COMPLETED','INPROCESS','PENDING','REJECTED','SUBMITTED') NOT NULL,
+  PRIMARY KEY (`applicationid`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `application`
+--
+
+LOCK TABLES `application` WRITE;
+/*!40000 ALTER TABLE `application` DISABLE KEYS */;
+INSERT INTO `application` VALUES (90000,5,3,NULL,7,9100,1,'2025-02-10',7,'IPS officer','SUBMITTED'),(20000,6,4,NULL,7,900,12,'2025-02-10',7,'Police Man','SUBMITTED'),(12000000,7,3,NULL,7,900000,1,'2025-02-10',7,'Businessman','SUBMITTED');
+/*!40000 ALTER TABLE `application` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-12 20:08:37
